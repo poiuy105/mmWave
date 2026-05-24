7459f653d38a6d414cad52e6b8298be9022b6127
poiuy105 <poiuy105@163.com>
2026-05-24 18:10:36 +0800

    Fix MACSTR format issues in ESP_LOGI

--- main/main.c
+++ main/main.c
@@ -41,12 +41,11 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                int32_t event_id, void* event_data)
 {
     if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-        ESP_LOGI(TAG, "Station "MACSTR" joined, AID=%d",
-                 MAC2STR(event->mac), event->aid);
+        wifi_event_ap_staconnected_t* evt = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG, "Station joined, AID=%d", evt->aid);
     } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-        wifi_event_ap_stadisconnected_t* disc_evt = (wifi_event_ap_stadisconnected_t*) event_data;
-        ESP_LOGI(TAG, "Station left, AID=%d", disc_evt->aid);
+        wifi_event_ap_stadisconnected_t* evt = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG, "Station left, AID=%d", evt->aid);
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
         esp_wifi_connect();
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
